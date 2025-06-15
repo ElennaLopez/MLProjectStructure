44c843d531e22343a6e3f02738817f0eb6eda65b
ElennaLopez <elopezvallejo98@gmail.com>
2025-06-15 10:18:59 +0200

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,9 @@ import sys
 import dill
 import pandas as pd
 import numpy as np
-from src.exception import CustomException
+from src.logger import CustomException, logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -20,7 +21,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params=None):
     """
     Evaluate multiple regression models and return their performance metrics.
     
@@ -38,6 +39,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
     
     for model_name, model in models.items():
         try:
+            params[model_name] = params.get(model_name, {})
+
+            gs = GridSearchCV(estimator=model, param_grid=params[model_name], cv=3, n_jobs=-1, verbose=2)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
+
+            logging.info(f"Training {model_name} with parameters: {gs.best_params_}")
             model.fit(X_train, y_train)
             y_pred = model.predict(X_test)
             r2_score_value = r2_score(y_test, y_pred)
